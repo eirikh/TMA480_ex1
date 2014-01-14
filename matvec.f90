36d54bfc5cee1e7a78e12363b83157d45830d21b
eirikh <eirik.hjertenaes@ntnu.no>
2014-01-14 12:52:06 +0100

    ready for tutorial

---
+++ matvec.f90
@@ -0,0 +1,31 @@
+program matvec
+
+real:: x(3), A(3,3),y(3)
+A=reshape((/0.3,0.7,0.5,0.4,0.1,0.5,0.3,0.2,0.0/),shape(A))
+x=reshape((/ 1.0,1.0,1.0/),shape(x))
+y=reshape((/ 0.0,0.0,0.0/),shape(x))
+
+write(*,*)" A"
+do i = 1,3
+   write(*,*) (A(i,j), j=1,3)
+enddo
+
+write(*,*)"x"
+do i = 1,3
+   write(*,*) x(i)
+enddo
+ 
+do i = 1,3
+   y(i)=0
+   do j = 1,3
+      y(i) = y(i) + A(i,j)*x(j)
+   enddo
+enddo
+
+write(*,*) "y"
+
+do i = 1,3
+   write(*,*) y(i)
+enddo
+
+endprogram
